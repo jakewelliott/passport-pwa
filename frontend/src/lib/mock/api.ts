d5b1b26c4a2a9feaaf782bb167e69887d4027b9e
adamwett <adamwettreich@gmail.com>
2025-01-27 16:21:47 -0500

    [Fix] fixed bug when loading stamp details

--- frontend/src/lib/mock/api.ts
+++ frontend/src/lib/mock/api.ts
@@ -1,24 +1,28 @@
-import parks from './parks';
-import { userStamps, userProfile } from './user';
+import parks from "./parks";
+import { userStamps, userProfile } from "./user";
 
-import type { Geopoint, Park, ParkCode } from './types';
+import type { Geopoint, Park, ParkCode } from "./types";
 
 // ADAM: We should design our API to allow us to do as much on the client side as possible.
 // Can't rely on the service layer if you're offline!
 // For now, this will do as a mock API.
 
 export const api = {
-  // Parks
-  getParks: () => parks,
-  getPark: (code: ParkCode) => parks.find((park: Park) => park.code === code),
+	// Parks
+	getParks: () => parks,
+	getPark: (code: ParkCode) => parks.find((park: Park) => park.code === code),
 
-  // Users
-  getUserByID: (userId: string) => userProfile,
-  getUserStampsByID: (userId: string) => userStamps,
+	// Users
+	getUserByID: (userId: string) => userProfile,
+	getUserStampsByID: (userId: string) => userStamps,
 
-  // Stamps
-  collectStamp: (userId: string, stampId: string, location: Geopoint | null) => {
-    console.log(stampId, userId, location);
-    console.error('Not mocked yet');
-  },
+	// Stamps
+	collectStamp: (
+		userId: string,
+		stampId: string,
+		location: Geopoint | null,
+	) => {
+		console.log(stampId, userId, location);
+		console.error("Not mocked yet");
+	},
 };
