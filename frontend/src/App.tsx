0f2510eb77c2e15d3da7b1fc51028ae8c1d3acea
alwettre <alwettre@ncsu.edu>
2025-01-27 15:50:53 -0500

    fixed splash screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,39 @@
-import { Routes, Route } from 'react-router-dom';
-import './styles/globals.css';
-import Header from './components/layout/header';
-import TabBar from './components/layout/tab-bar';
-import Locations from './app/locations/index';
-import Stamps from './app/stamps/index';
-import More from './app/more/index';
-import LocationDetail from './app/locations/location-detail';
-import { AppInfo } from './app/more/app-info';
-import { IconLegend } from './app/more/icon-legend';
-import { BucketList } from './app/more/bucket-list';
+import { Routes, Route } from "react-router-dom";
+import "./styles/globals.css";
+import Header from "./components/layout/header";
+import TabBar from "./components/layout/tab-bar";
+import Locations from "./app/locations/index";
+import Stamps from "./app/stamps/index";
+import More from "./app/more/index";
+import LocationDetail from "./app/locations/location-detail";
+import { AppInfo } from "./app/more/app-info";
+import { IconLegend } from "./app/more/icon-legend";
+import { BucketList } from "./app/more/bucket-list";
+import { SplashScreenWrapper } from "./components/splash-screen";
 
 export default function App() {
-  return (
-    <div className='app'>
-      <Header />
-      <TabBar />
-      <main className='flex-grow pb-16'>
-        <Routes>
-          <Route path='/locations' element={<Locations />} />
-          <Route path='/stamps' element={<Stamps />} />
-          <Route path='/more' element={<More />} />
-          <Route path='/more/app-info' element={<AppInfo />} />
-          <Route path='/more/bucket-list' element={<BucketList />} />
-          <Route path='/more/icon-legend' element={<IconLegend />} />
-          <Route path='/locations/location-detail/:locationAbbreviation' element={<LocationDetail />} />
-        </Routes>
-      </main>
-    </div>
-  );
+	console.log("hi from app!");
+
+	return (
+		<SplashScreenWrapper>
+			<div className="app">
+				<Header />
+				<TabBar />
+				<main className="flex-grow pb-16">
+					<Routes>
+						<Route path="/locations" element={<Locations />} />
+						<Route path="/stamps" element={<Stamps />} />
+						<Route path="/more" element={<More />} />
+						<Route path="/more/app-info" element={<AppInfo />} />
+						<Route path="/more/bucket-list" element={<BucketList />} />
+						<Route path="/more/icon-legend" element={<IconLegend />} />
+						<Route
+							path="/locations/location-detail/:locationAbbreviation"
+							element={<LocationDetail />}
+						/>
+					</Routes>
+				</main>
+			</div>
+		</SplashScreenWrapper>
+	);
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,37 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { queryClient } from './lib/tanstack-local-storage.ts';
-import App from './App.tsx';
-import { register as registerServiceWorker } from './lib/service-worker.ts';
-import './styles/globals.css';
-import './styles/fonts.css';
-import SplashScreen from './components/splash-screen.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "./lib/tanstack-local-storage.ts";
+import App from "./App.tsx";
+import { register as registerServiceWorker } from "./lib/service-worker.ts";
+import "./styles/globals.css";
+import "./styles/fonts.css";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 if (!rootElement) {
-  throw new Error('Root element not found');
+	throw new Error("Root element not found");
 }
 
-// TODO: Implement splash screen, lives here for now
-let loaded = false;
-setTimeout(() => {
-  loaded = true;
-}, 2000);
-const AppSplash = () => (loaded ? <App /> : <SplashScreen />);
+console.log("hi from main!");
 
 ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AppSplash />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </BrowserRouter>
-    </QueryClientProvider>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<App />
+				<ReactQueryDevtools initialIsOpen={false} />
+			</BrowserRouter>
+		</QueryClientProvider>
+	</React.StrictMode>,
 );
 
 registerServiceWorker();
