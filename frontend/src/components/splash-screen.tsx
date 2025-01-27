81b85857f354eef032f12a4f7e6dc23224422a9b
alwettre <alwettre@ncsu.edu>
2025-01-27 14:56:38 -0500

    [Fix] Add import dirs to vite and fixed splash screen component

--- frontend/src/components/splash-screen.tsx
+++ frontend/src/components/splash-screen.tsx
@@ -1,24 +1,38 @@
-export default () => (
-  <div
-    className='fixed inset-0 flex flex-col items-center bg-no-repeat'
-    style={{
-      backgroundImage: "url('./photos/CRMO_FrontCover.jpg')",
-      backgroundSize: '150%',
-      backgroundPosition: 'center 25%',
-      zIndex: 9999,
-    }}
-  >
-    <div className='flex h-2/4 flex-col items-center justify-center'>
-      <img
-        src='./DPRLogoWhite.svg'
-        alt='North Carolina Department of Parks and Rec White Logo'
-        width='136'
-        height='103'
-      />
-      <h3>North Carolina State Parks</h3>
-      <p className='script' style={{ paddingTop: 30 }}>
-        Passport
-      </p>
-    </div>
-  </div>
-);
+import { useState, useEffect } from "react";
+
+export default function SplashScreen() {
+	const [loaded, setLoaded] = useState(false);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setLoaded(true);
+		}, 2000);
+	}, []);
+
+	if (!loaded) return null;
+
+	return (
+		<div
+			className="fixed inset-0 flex flex-col items-center bg-no-repeat"
+			style={{
+				backgroundImage: "url('./photos/CRMO_FrontCover.jpg')",
+				backgroundSize: "150%",
+				backgroundPosition: "center 25%",
+				zIndex: 9999,
+			}}
+		>
+			<div className="flex h-2/4 flex-col items-center justify-center">
+				<img
+					src="./DPRLogoWhite.svg"
+					alt="North Carolina Department of Parks and Rec White Logo"
+					width="136"
+					height="103"
+				/>
+				<h3>North Carolina State Parks</h3>
+				<p className="script" style={{ paddingTop: 30 }}>
+					Passport
+				</p>
+			</div>
+		</div>
+	);
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "node:fs";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-	],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 	server: {
 		host: "0.0.0.0",
 		port: 5173,
