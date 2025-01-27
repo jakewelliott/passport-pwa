045866f228698aeec7e9393951d29cad5132ef60
alwettre <alwettre@ncsu.edu>
2025-01-26 21:23:01 -0500

    [Fix] Update stamp-related imports and add dummy stamp type

--- frontend/src/lib/mock/user.ts
+++ frontend/src/lib/mock/user.ts
@@ -1,23 +1,29 @@
-import type { UserParkVisit, UserProfile } from '@/lib/mock/types';
-import userStampsJson from '@/lib/mock/user_stamps.json';
-import userParkVisitsJson from '@/lib/mock/user_park_visits.json';
-import dummyMapper from '@/lib/dummy-mapper';
+import type { UserParkVisit, UserProfile, UserStamp } from "@/lib/mock/types";
+import userStampsJson from "@/lib/mock/user_stamps.json";
+import userParkVisitsJson from "@/lib/mock/user_park_visits.json";
+import dummyMapper from "@/lib/dummy-mapper";
 
 const dummyVisit: UserParkVisit = {
-  code: 'DUMM',
-  timestamp: new Date(),
+	code: "DUMM",
+	timestamp: new Date(),
 };
 
 export const userParkVisits = userParkVisitsJson.map(dummyMapper(dummyVisit));
 
 const dummyProfile: UserProfile = {
-  username: 'dummyuser',
-  email: 'dummyuser@test.com',
-  password: 'dummyuser',
-  stamps: [],
-  visits: [],
+	username: "dummyuser",
+	email: "dummyuser@test.com",
+	password: "dummyuser",
+	stamps: [],
+	visits: [],
 };
 
-export const userStamps = userStampsJson.map(dummyMapper(dummyProfile));
+const dummyStamp: UserStamp = {
+	code: "DUMM",
+	timestamp: new Date(),
+	location: null,
+};
+
+export const userStamps = userStampsJson.map(dummyMapper(dummyStamp));
 
 export const userProfile = dummyProfile;
