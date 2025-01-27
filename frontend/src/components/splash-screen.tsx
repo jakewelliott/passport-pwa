cf51a495f99698fbf3150b796a7e5819f40bf5ff
alwettre <alwettre@ncsu.edu>
2025-01-27 16:45:08 -0500

    [Lint] npm run fix

--- frontend/src/components/splash-screen.tsx
+++ frontend/src/components/splash-screen.tsx
@@ -1,44 +1,44 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function SplashScreen() {
-	return (
-		<div
-			className="fixed inset-0 flex flex-col items-center bg-no-repeat"
-			style={{
-				backgroundImage: "url('/photos/CRMO_FrontCover.jpg')",
-				backgroundSize: "150%",
-				backgroundPosition: "center 25%",
-				zIndex: 9999,
-			}}
-		>
-			<div className="flex h-2/4 flex-col items-center justify-center">
-				<img
-					src="/DPRLogoWhite.svg"
-					alt="North Carolina Department of Parks and Rec White Logo"
-					width="136"
-					height="103"
-				/>
-				<h3>North Carolina State Parks</h3>
-				<p className="script" style={{ paddingTop: 30 }}>
-					Passport
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className='fixed inset-0 flex flex-col items-center bg-no-repeat'
+      style={{
+        backgroundImage: "url('/photos/CRMO_FrontCover.jpg')",
+        backgroundSize: '150%',
+        backgroundPosition: 'center 25%',
+        zIndex: 9999,
+      }}
+    >
+      <div className='flex h-2/4 flex-col items-center justify-center'>
+        <img
+          src='/DPRLogoWhite.svg'
+          alt='North Carolina Department of Parks and Rec White Logo'
+          width='136'
+          height='103'
+        />
+        <h3>North Carolina State Parks</h3>
+        <p className='script' style={{ paddingTop: 30 }}>
+          Passport
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export function SplashScreenWrapper(props: React.PropsWithChildren) {
-	const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setLoading(false);
-		}, 2000);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
 
-		return () => clearTimeout(timer);
-	}, []);
+    return () => clearTimeout(timer);
+  }, []);
 
-	return loading ? <SplashScreen /> : <>{props.children}</>;
+  return loading ? <SplashScreen /> : <>{props.children}</>;
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,31 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { queryClient } from "./lib/tanstack-local-storage.ts";
-import App from "./App.tsx";
-import { register as registerServiceWorker } from "./lib/service-worker.ts";
-import "./styles/globals.css";
-import "./styles/fonts.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from './lib/tanstack-local-storage.ts';
+import App from './App.tsx';
+import { register as registerServiceWorker } from './lib/service-worker.ts';
+import './styles/globals.css';
+import './styles/fonts.css';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 
 if (!rootElement) {
-	throw new Error("Root element not found");
+  throw new Error('Root element not found');
 }
 
-console.log("hi from main!");
+console.log('hi from main!');
 
 ReactDOM.createRoot(rootElement).render(
-	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<BrowserRouter>
-				<App />
-				<ReactQueryDevtools initialIsOpen={false} />
-			</BrowserRouter>
-		</QueryClientProvider>
-	</React.StrictMode>,
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
 
 registerServiceWorker();
