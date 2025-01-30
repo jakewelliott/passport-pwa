2bc9e0652b762ca25e46e7072652a2365668b29a
adamwett <adamwettreich@gmail.com>
2025-01-30 10:56:17 -0500

    [Feature] Add Geolocation and Park Detection Hooks

--- frontend/src/components/scratchpad.tsx
+++ frontend/src/components/scratchpad.tsx
@@ -1,29 +1,50 @@
-import { TrailIcon } from './common/trail-icons';
+import type { Geopoint } from "@/lib/mock/types";
+import { TrailIcon } from "./common/trail-icons";
+import { useParkCheck } from "@/hooks/useParkCheck";
 
 const IconBox = ({ children }: { children: React.ReactNode }) => {
-  return <div className='flex flex-row items-center gap-2 rounded-md bg-secondary_lightgreen p-2'>{children}</div>;
+	return (
+		<div className="flex flex-row items-center gap-2 rounded-md bg-secondary_lightgreen p-2">
+			{children}
+		</div>
+	);
+};
+
+const spoofLocation: Geopoint = {
+	latitude: 35.6679,
+	longitude: -80.9443,
 };
 
 export const Scratchpad = () => {
-  console.log('Scratchpad');
-  return (
-    <div className='flex flex-col items-center gap-2'>
-      <h1>Scratchpad</h1>
-      <IconBox>
-        <TrailIcon iconName='Hiking-Red' size='sm' showText />
-        <TrailIcon iconName='Hiking-Red' size='sm' showText />
-        <TrailIcon iconName='Hiking-Red' size='sm' showText />
-      </IconBox>
-      <IconBox>
-        <TrailIcon iconName='Camping-Green' size='md' />
-        <TrailIcon iconName='Camping-Green' size='md' />
-        <TrailIcon iconName='Camping-Green' size='md' />
-      </IconBox>
-      <IconBox>
-        <TrailIcon iconName='BoatRamp-Blue' size='lg' showText />
-        <TrailIcon iconName='BoatRamp-Blue' size='lg' showText />
-        <TrailIcon iconName='BoatRamp-Blue' size='lg' showText />
-      </IconBox>
-    </div>
-  );
+	const { park, isLoading } = useParkCheck(spoofLocation);
+
+	return (
+		<div className="flex flex-col items-center gap-2">
+			<h1>Scratchpad</h1>
+			<IconBox>
+				<TrailIcon iconName="Hiking-Red" size="sm" showText />
+				<TrailIcon iconName="Hiking-Red" size="sm" showText />
+				<TrailIcon iconName="Hiking-Red" size="sm" showText />
+			</IconBox>
+			<IconBox>
+				<TrailIcon iconName="Camping-Green" size="md" />
+				<TrailIcon iconName="Camping-Green" size="md" />
+				<TrailIcon iconName="Camping-Green" size="md" />
+			</IconBox>
+			<IconBox>
+				<TrailIcon iconName="BoatRamp-Blue" size="lg" showText />
+				<TrailIcon iconName="BoatRamp-Blue" size="lg" showText />
+				<TrailIcon iconName="BoatRamp-Blue" size="lg" showText />
+			</IconBox>
+			<div>
+				{isLoading ? (
+					<p>Loading...</p>
+				) : park ? (
+					<p>{park.name}</p>
+				) : (
+					<p>You don't seem to be in a park</p>
+				)}
+			</div>
+		</div>
+	);
 };
