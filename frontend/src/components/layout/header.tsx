c31700370a117d28c48f6eb57c865a4bd7d6c4b3
jwellio2 <jwellio2@ncsu.edu>
2025-02-07 10:42:47 -0500

    fixed nav bar issues

--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -14,19 +14,39 @@ const BackButton = ({ hidden }: { hidden: boolean }) => {
   );
 };
 
+const formatTitle = (title: string) => {
+  return title
+    .split('-')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+};
+
 const Header = () => {
   const location = useLocation();
-  const locationName = location.pathname.split('/').pop()?.toUpperCase();
+  var navBarTitle = "NC Parks";
+  const pathPieces = location.pathname.split('/');
+  const lastPiece = pathPieces.pop();
+  if (pathPieces.includes("stamps"))
+    navBarTitle = "My Stamps"
+  else if (pathPieces.includes("more") && !lastPiece?.includes("more"))
+    navBarTitle = formatTitle(lastPiece!)
+
+  const isTopLevel = topLevelCheck(location.pathname);
 
   return (
     <div className='sticky top-0 right-0 left-0' style={{ zIndex: 9999 }}>
-      <header className='flex items-center justify-between bg-secondary_darkteal p-4' style={{ height: '50px' }}>
-        <BackButton hidden={topLevelCheck(location.pathname)} />
-        <h4 className='text-system_white'>{locationName}</h4>
-        <div />
+      <header className='flex items-center justify-center bg-secondary_darkteal p-4 relative' style={{ height: '50px' }}>
+        {!isTopLevel && (
+          <div className='absolute left-4'>
+            <BackButton hidden={false} />
+          </div>
+        )}
+        <h4 className='text-system_white'>{navBarTitle}</h4>
+        <div className='absolute right-4 w-[70px]' /> {/* Placeholder for balance */}
       </header>
     </div>
   );
 };
+
 
 export default Header;
