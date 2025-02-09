7c8f6f20e5da81212cd15255d48e1771cfe83ec5
V Mehra <ymehra@ncsu.edu>
2025-02-08 21:05:47 -0500

    Add location repository tests

---
+++ backend/tests/UnitTests/DigitalPassportBackend.UnitTests/Persistence/Repository/LocationsRepositoryTests.cs
@@ -0,0 +1,49 @@
+using DigitalPassportBackend.Errors;
+using DigitalPassportBackend.Persistence.Database;
+using DigitalPassportBackend.Persistence.Repository;
+using DigitalPassportBackend.UnitTests.TestUtils.Constants;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalPassportBackend.UnitTests.Persistence.Repository;
+public class LocationsRepositoryTests
+{
+    private readonly DigitalPassportDbContext _db;
+
+    private readonly LocationsRepository _repo;
+
+    public LocationsRepositoryTests()
+    {
+        // Initialize the testing DB.
+        var options = new DbContextOptionsBuilder<DigitalPassportDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+            _db = new(options);
+
+        // Populate the testing DB.
+        _db.Parks.AddRange(Constants.Parks);
+        _db.SaveChanges();
+
+        // Initialize the repository.
+        _repo = new LocationsRepository(_db);
+    }
+
+    [Fact]
+    public void GetByAbbreviation_ReturnsPark_WhenLocationExists()
+    {
+        // Action.
+        var park1 = _repo.GetByAbbreviation("CABE");
+        var park2 = _repo.GetByAbbreviation("EBII");
+
+        // Assert.
+        Assert.Equal(Constants.Parks[0], park1);
+        Assert.Equal(Constants.Parks[1], park2);
+    }
+
+    [Fact]
+    public void GetByAbbreviation_ThrowsNotFoundException_WhenLocationNotFound()
+    {
+        // Action and assertion.
+        Assert.Throws<NotFoundException>(() => _repo.GetByAbbreviation("DMV"));
+    }
+}
