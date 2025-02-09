d741d94538ebba2e2ea7cf054241d492521c8bf2
ymehra <ymehra@ncsu.edu>
2025-02-08 20:37:19 -0500

    Bucket list repository tests

--- backend/tests/UnitTests/DigitalPassportBackend.UnitTests/TestUtils/Constants/Constants.Park.cs
+++ backend/tests/UnitTests/DigitalPassportBackend.UnitTests/TestUtils/Constants/Constants.Park.cs
@@ -4,23 +4,43 @@ namespace DigitalPassportBackend.UnitTests.TestUtils.Constants
 {
     public static partial class Constants
     {
-        public static class Park
+        public static List<Park> Parks = new()
         {
-            public const int id = 45;
-            public const string parkName = "Carolina Beach State Park";
-            public const string parkAbbreviation = "CABE";
-            public const ParkType parkType = ParkType.SPA;
-            public const string city = "Carolina Beach";
-            public static readonly object coordinates = new { longitude = -77.9066, latitude = 34.0472 };
-            public const long phone = 9104588206;
-            public const string email = "carolina.beach@ncparks.gov";
-            public const string stampImage = "";
-            public const string establishedYear = "1969";
-            public const string landmark = "Sugarloaf Dune, which has helped people find their way since 1663 and offers a great view of the Cape Fear River";
-            public const string youCanFind = "the Venus flytrap, one of the rarest species of plants that eats bugs.";
-            public const string trails = "■ 9 trails\n\n■ 1 wheelchair-accessible trail\n\n■ Kids TRACK Trail (follows Snow’s Cut Trail)\n\n■ 8.75 miles of hiking\n\n■ 1 mile of biking";
-            public const string accesses = null;
-            public const string website = "https://www.ncparks.gov/state-parks/carolina-beach-state-park";            
-        }
+            new() {
+                id = 45,
+                parkName = "Carolina Beach State Park",
+                parkAbbreviation = "CABE",
+                parkType = ParkType.SPA,
+                city = "Carolina Beach",
+                coordinates = new(-77.9066, 34.0472),
+                phone = 9104588206,
+                email = "carolina.beach@ncparks.gov",
+                stampImage = "",
+                establishedYear = "1969",
+                landmark = "Sugarloaf Dune, which has helped people find their way since 1663 and offers a great view of the Cape Fear River",
+                youCanFind = "the Venus flytrap, one of the rarest species of plants that eats bugs.",
+                trails = "■ 9 trails\n\n■ 1 wheelchair-accessible trail\n\n■ Kids TRACK Trail (follows Snow’s Cut Trail)\n\n■ 8.75 miles of hiking\n\n■ 1 mile of biking",
+                accesses = null,
+                website = "https://www.ncparks.gov/state-parks/carolina-beach-state-park"
+            },
+            new()
+            {
+                id = 20,
+                parkName = "Engineering Building II",
+                parkAbbreviation = "EBII",
+                parkType = ParkType.SNA,
+                city = "Raleigh",
+                coordinates = new(35.7720, -78.6740),
+                phone = 9195152336,
+                email = "engineering@ncsu.edu",
+                stampImage = "",
+                establishedYear = "1984",
+                landmark = "CSC and ECE departments",
+                youCanFind = "Stressed engineering students",
+                trails = "trail information",
+                accesses = null,
+                website = "https://www.nscu.edu"
+            }
+        };
     }
 }
