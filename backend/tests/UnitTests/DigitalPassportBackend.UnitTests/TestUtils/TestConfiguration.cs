599f6e9af3b9134751fa8bda9fc44f6739a02113
V Mehra <ymehra@ncsu.edu>
2025-02-10 18:08:36 -0500

    Possible fix for workflows

--- backend/tests/UnitTests/DigitalPassportBackend.UnitTests/TestUtils/TestConfiguration.cs
+++ backend/tests/UnitTests/DigitalPassportBackend.UnitTests/TestUtils/TestConfiguration.cs
@@ -5,7 +5,11 @@ public static class TestConfiguration
 {
     public static IConfiguration GetConfiguration()
     {
+# if DEBUG
         DotEnv.Load(Path.Combine(Directory.GetCurrentDirectory(), "../../../../../../../.env"));
+# else
+        DotEnv.Load(Path.Combine(Directory.GetCurrentDirectory(), ".env"));
+# endif
         return new ConfigurationBuilder()
             .AddEnvironmentVariables()
             .Build();
