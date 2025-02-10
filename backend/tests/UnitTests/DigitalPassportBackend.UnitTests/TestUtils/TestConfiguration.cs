052b780fd9336818a52e7461ac1ea8a39bac847e
V Mehra <ymehra@ncsu.edu>
2025-02-10 18:31:45 -0500

    a

--- backend/tests/UnitTests/DigitalPassportBackend.UnitTests/TestUtils/TestConfiguration.cs
+++ backend/tests/UnitTests/DigitalPassportBackend.UnitTests/TestUtils/TestConfiguration.cs
@@ -5,7 +5,7 @@ public static class TestConfiguration
 {
     public static IConfiguration GetConfiguration()
     {
-        DotEnv.Load(Path.Combine(Directory.GetCurrentDirectory(), "../.env"));
+        DotEnv.Load(Path.Combine(Directory.GetCurrentDirectory(), ".env"));
         return new ConfigurationBuilder()
             .AddEnvironmentVariables()
             .Build();
