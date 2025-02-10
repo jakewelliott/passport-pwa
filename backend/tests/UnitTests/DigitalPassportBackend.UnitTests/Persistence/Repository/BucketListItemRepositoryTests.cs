5507774feab6320ed3bb364ae23e108693dbafad
ymehra <ymehra@ncsu.edu>
2025-02-09 21:56:23 -0500

    Rename test data class

--- backend/tests/UnitTests/DigitalPassportBackend.UnitTests/Persistence/Repository/BucketListItemRepositoryTests.cs
+++ backend/tests/UnitTests/DigitalPassportBackend.UnitTests/Persistence/Repository/BucketListItemRepositoryTests.cs
@@ -1,4 +1,3 @@
-using DigitalPassportBackend.Domain;
 using DigitalPassportBackend.Persistence.Database;
 using DigitalPassportBackend.Persistence.Repository;
 using DigitalPassportBackend.UnitTests.TestUtils;
@@ -21,7 +20,7 @@ public class BucketListItemRepositoryTests
         _db = new(options);
 
         // Populate the testing DB.
-        _db.BucketListItems.AddRange(RepositoryTestData.BucketList);
+        _db.BucketListItems.AddRange(TestData.BucketList);
         _db.SaveChanges();
 
         // Initialize the repository.
@@ -32,12 +31,12 @@ public class BucketListItemRepositoryTests
     public void GetByLocationId_ReturnsPopulatedList_WhenLocationExists()
     {
         // Action.
-        var items = _repo.GetByLocationId(RepositoryTestData.Parks[0].id);
+        var items = _repo.GetByLocationId(TestData.Parks[0].id);
 
         // Assert.
         Assert.True(items.Count == 2);
-        Assert.Contains(items, i => i == RepositoryTestData.BucketList[0]);
-        Assert.Contains(items, i => i == RepositoryTestData.BucketList[2]);
+        Assert.Contains(items, i => i == TestData.BucketList[0]);
+        Assert.Contains(items, i => i == TestData.BucketList[2]);
     }
 
     [Fact]
