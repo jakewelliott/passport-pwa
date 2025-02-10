3cc3447877efbf32969f79dc5efd3bb872392a6d
jwellio2 <jwellio2@ncsu.edu>
2025-02-09 22:46:36 -0500

    added repository interface

--- backend/src/DigitalPassportBackend/Persistence/Repository/CompletedBucketListItemRepository.cs
+++ backend/src/DigitalPassportBackend/Persistence/Repository/CompletedBucketListItemRepository.cs
@@ -1,13 +1,24 @@
 using DigitalPassportBackend.Domain;
+using DigitalPassportBackend.Errors;
 using DigitalPassportBackend.Persistence.Database;
 
 namespace DigitalPassportBackend.Persistence.Repository;
-public class CompletedBucketListItemRepository(DigitalPassportDbContext digitalPassportDbContext)
+public class CompletedBucketListItemRepository(DigitalPassportDbContext digitalPassportDbContext) : IRepository<CompletedBucketListItem>
 {
     private readonly DigitalPassportDbContext _digitalPassportDbContext = digitalPassportDbContext;
 
     public Park? GetByAbbreviation(string abbreviation)
     {
         return _digitalPassportDbContext.Parks.Where(l => l.parkAbbreviation.Equals(abbreviation)).Single();
+    }
+
+    public CompletedBucketListItem GetById(int id)
+    {
+        var result = _digitalPassportDbContext.CompletedBucketListItems.Where(a => a.id.Equals(id)).SingleOrDefault();
+        if (result is null)
+        {
+            throw new NotFoundException($"ParkPhoto not found with id {id}");
+        }
+        return result;
     }
 }
