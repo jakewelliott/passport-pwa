8aff15f74f3ccfcfbd107d97ddd8dcac98f250ec
V Mehra <ymehra@ncsu.edu>
2025-02-10 01:20:34 -0500

    Add locations repository tests

--- backend/tests/UnitTests/DigitalPassportBackend.UnitTests/Persistence/Repository/LocationsRepositoryTests.cs
+++ backend/tests/UnitTests/DigitalPassportBackend.UnitTests/Persistence/Repository/LocationsRepositoryTests.cs
@@ -1,3 +1,4 @@
+using DigitalPassportBackend.Domain;
 using DigitalPassportBackend.Errors;
 using DigitalPassportBackend.Persistence.Database;
 using DigitalPassportBackend.Persistence.Repository;
@@ -46,4 +47,91 @@ public class LocationsRepositoryTests
         // Action and assertion.
         Assert.Throws<NotFoundException>(() => _repo.GetByAbbreviation("DMV"));
     }
+
+    [Fact]
+    public void GetById_ReturnsPark_WhenLocationExists()
+    {
+        // Action.
+        var item = _repo.GetById(TestData.Parks[0].id);
+
+        // Assert.
+        Assert.Equal(TestData.Parks[0], item);
+    }
+
+    [Fact]
+    public void GetById_ThrowsNotFoundException_WhenLocationDNE()
+    {
+        // Action and assert.
+        Assert.Throws<NotFoundException>(() => _repo.GetById(5));
+    }
+
+    [Fact]
+    public void Delete_ReturnsDeletedPark_WhenLocationExists()
+    {
+        // Action.
+        var item = _repo.Delete(TestData.Parks[0].id);
+
+        // Assert.
+        Assert.Equal(1, _db.Parks.Count());
+        Assert.DoesNotContain(TestData.Parks[0], _db.Parks);
+    }
+
+    [Fact]
+    public void Delete_ThrowsNotFoundException_WhenLocationDNE()
+    {
+        // Action and assert.
+        Assert.Throws<NotFoundException>(() => _repo.Delete(5));
+        Assert.Equal(2, _db.Parks.Count());
+    }
+
+    [Fact]
+    public void Update_ReturnsExistingItem_WhenLocationExists()
+    {
+        // Prepare updated park.
+        var newItem = TestData.Parks[0];
+        newItem.email = "new email";
+
+        // Action.
+        var oldItem = _repo.Update(newItem);
+
+        // Assert.
+        Assert.Equal(2, _db.Parks.Count());
+        Assert.Equal(TestData.Parks[0], oldItem);
+        Assert.Contains(newItem, _db.Parks);
+    }
+
+    [Fact]
+    public void Update_ThrowsNotFoundException_WhenLocationDNE()
+    {
+        // Action and assert.
+        Assert.Throws<NotFoundException>(() => _repo.Update(NewPark));
+        Assert.Equal(2, _db.Parks.Count());
+    }
+
+    [Fact]
+    public void Count_ReturnsNumberOfLocations()
+    {
+        // Action and assert.
+        Assert.Equal(2, _repo.Count());
+    }
+
+    [Fact]
+    public void Create_ReturnsNewPark_IfLocationDNE()
+    {
+        // Action.
+        var item = _repo.Create(NewPark);
+
+        // Assert.
+        Assert.Equal(3, _db.Parks.Count());
+        Assert.Contains(NewPark, _db.Parks);
+    }
+
+    private static readonly Park NewPark = new()
+    {
+        id = 5,
+        parkName = "New York City",
+        parkAbbreviation = "NYC",
+        parkType = ParkType.SPA,
+        website = "google.com"
+    };
 }
