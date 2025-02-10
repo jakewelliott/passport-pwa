9f38aad3447668d163807434e28c9937f417e5fb
ymehra <ymehra@ncsu.edu>
2025-02-10 01:44:00 -0500

    Add bucket list item repository tests

--- backend/tests/UnitTests/DigitalPassportBackend.UnitTests/Persistence/Repository/BucketListItemRepositoryTests.cs
+++ backend/tests/UnitTests/DigitalPassportBackend.UnitTests/Persistence/Repository/BucketListItemRepositoryTests.cs
@@ -1,3 +1,5 @@
+using DigitalPassportBackend.Domain;
+using DigitalPassportBackend.Errors;
 using DigitalPassportBackend.Persistence.Database;
 using DigitalPassportBackend.Persistence.Repository;
 using DigitalPassportBackend.UnitTests.TestUtils;
@@ -48,4 +50,88 @@ public class BucketListItemRepositoryTests
         // Assert.
         Assert.False(items.Any());
     }
+
+    [Fact]
+    public void GetById_ReturnsBucketListItem_WhenBucketListItemExists()
+    {
+        // Action.
+        var item = _repo.GetById(TestData.BucketList[1].id);
+
+        // Assert.
+        Assert.Equal(TestData.BucketList[1], item);
+    }
+
+    [Fact]
+    public void GetById_ThrowsNotFoundException_WhenBucketListItemDNE()
+    {
+        // Action and assert.
+        Assert.Throws<NotFoundException>(() => _repo.GetById(5));
+    }
+
+    [Fact]
+    public void Delete_ReturnsDeletedBucketListItem_WhenItemExists()
+    {
+        // Action.
+        var item = _repo.Delete(TestData.BucketList[0].id);
+
+        // Assert.
+        Assert.Equal(2, _db.BucketListItems.Count());
+        Assert.DoesNotContain(TestData.BucketList[0], _db.BucketListItems);
+    }
+
+    [Fact]
+    public void Delete_ThrowsNotFoundException_WhenItemDNE()
+    {
+        // Action and assert.
+        Assert.Throws<NotFoundException>(() => _repo.Delete(5));
+    }
+
+    [Fact]
+    public void Update_ReturnsExistingItem_WhenItemExists()
+    {
+        // Prepare updated bucket list item.
+        var newItem = TestData.BucketList[0];
+        newItem.task = "new task description";
+
+        // Action.
+        var oldItem = _repo.Update(newItem);
+
+        // Assert.
+        Assert.Equal(3, _db.BucketListItems.Count());
+        Assert.Equal(TestData.BucketList[0], oldItem);
+        Assert.Contains(newItem, _db.BucketListItems);
+    }
+
+    [Fact]
+    public void Update_ThrowsNotFoundException_WhenItemDNE()
+    {
+        // Action and assert.
+        Assert.Throws<NotFoundException>(() => _repo.Update(NewItem));
+        Assert.Equal(3, _db.BucketListItems.Count());
+    }
+
+    [Fact]
+    public void Count_ReturnsNumberOfItems()
+    {
+        Assert.Equal(3, _repo.Count());
+    }
+
+    [Fact]
+    public void Create_ReturnsNewItem_IfItemDNE()
+    {
+        // Action.
+        var item = _repo.Create(NewItem);
+
+        // Assert.
+        Assert.Equal(4, _db.BucketListItems.Count());
+        Assert.Equal(NewItem, item);
+        Assert.Contains(NewItem, _db.BucketListItems);
+    }
+
+    private static readonly BucketListItem NewItem = new()
+    {
+        id = 5,
+        task = "Task 2 at EB2",
+        park = TestData.Parks[1]
+    };
 }
