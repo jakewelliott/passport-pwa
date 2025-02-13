126f9ae31c38138d33102e490da00b29d4b6e279
alwettre <alwettre@ncsu.edu>
2025-02-12 22:11:20 -0500

    [Fix] Refactor routes, stamps, and UI components for improved navigation and user experience

--- frontend/src/components/tab-bar.tsx
+++ frontend/src/components/tab-bar.tsx
@@ -9,18 +9,16 @@ const TabBar = () => {
 	if (!user || isLoading) return null;
 
 	const location = useLocation();
-	const allTabs = [
-		{ name: 'Stamps', path: '/stamps', icon: <FaStamp size={'24px'} />, roles: ['visitor'] },
-		{ name: 'Locations', path: '/locations', icon: <TbMap size={'24px'} />, roles: ['visitor', 'admin'] },
-		{ name: 'More', path: '/more', icon: <MdMoreHoriz size={'24px'} />, roles: ['visitor', 'admin'] },
+	const tabs = [
+		{ name: 'Locations', path: '/locations', icon: <TbMap size={'24px'} /> },
+		{ name: 'Stamps', path: '/stamps', icon: <FaStamp size={'24px'} /> },
+		{ name: 'More', path: '/more', icon: <MdMoreHoriz size={'24px'} /> },
 	];
-
-	const visibleTabs = allTabs.filter(tab => tab.roles.includes(user.role || 'visitor'));
 
 	return (
 		<nav className='fixed right-0 bottom-0 left-0 bg-secondary_darkteal'>
 			<ul className='flex h-16 items-center justify-around'>
-				{visibleTabs.map((tab) => (
+				{tabs.map((tab) => (
 					<li key={tab.name}>
 						<Link to={tab.path} style={{ textDecoration: 'none' }}>
 							<div
