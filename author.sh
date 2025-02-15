6acb055927db4e310e535deb03a83c16350e3cf7
alwettre <alwettre@ncsu.edu>
2025-02-15 14:06:12 -0500

    update author

--- author.sh
+++ author.sh
@@ -1,7 +1,9 @@
-git filter-repo --force --commit-callback '
-    if commit.author_name == b"adamwett":
-        commit.author_name = b"alwettre"
-        commit.author_email = b"alwettre@ncsu.edu"
-        commit.committer_name = b"alwettre"
-        commit.committer_email = b"alwettre@ncsu.edu"
-' 
+git filter-branch --env-filter '
+if [ "$GIT_AUTHOR_NAME" = "adamwett" ]
+then
+    export GIT_AUTHOR_NAME="alwettre"
+    export GIT_AUTHOR_EMAIL="alwettre@ncsu.edu"
+    export GIT_COMMITTER_NAME="alwettre"
+    export GIT_COMMITTER_EMAIL="alwettre@ncsu.edu"
+fi
+' --tag-name-filter cat -- --all
