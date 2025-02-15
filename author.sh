f97490577f189454bc231f2b96db9d840888b142
alwettre <alwettre@ncsu.edu>
2025-02-15 15:19:34 -0500

    iteration 2 init env example

--- author.sh
+++
@@ -1,9 +0,0 @@
-git filter-branch --env-filter '
-if [ "$GIT_AUTHOR_NAME" = "adamwett" ]
-then
-    export GIT_AUTHOR_NAME="alwettre"
-    export GIT_AUTHOR_EMAIL="alwettre@ncsu.edu"
-    export GIT_COMMITTER_NAME="alwettre"
-    export GIT_COMMITTER_EMAIL="alwettre@ncsu.edu"
-fi
-' --tag-name-filter cat -- --all
