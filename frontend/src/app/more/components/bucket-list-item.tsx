79546e81b96e6c6d5b13e51f55a1686da7ecaf7e
jwellio2 <jwellio2@ncsu.edu>
2025-02-16 08:49:50 -0500

    better jest coverage

--- frontend/src/app/more/components/bucket-list-item.tsx
+++ frontend/src/app/more/components/bucket-list-item.tsx
@@ -1,60 +1,72 @@
 import { FaRegCheckSquare, FaRegSquare } from 'react-icons/fa';
 import type { Park } from '@/lib/mock/types';
-export const BucketListItem = () => {
-  const park: Park = {
-    abbreviation: 'CABE',
-    parkName: 'Sample Data',
-    addresses: [
-      {
-        title: 'Secondary Address:',
-        addressLineOne: '1234 Main St',
-        city: 'Raleigh',
-        state: 'NC',
-        zipcode: 27606,
-        addressLineTwo: ''
-      },
-      {
-        title: 'Tertiary Address:',
-        addressLineOne: '1234 Main St',
-        city: 'Raleigh',
-        state: 'NC',
-        zipcode: 27606,
-        addressLineTwo: ''
-      },
-    ],
-    coordinates: { latitude: 35.2023, longitude: -78.9761 },
-    phone: 5555555555,
-    email: 'email@ncparks.gov',
-    website: 'ncparks.gov',
-    establishedYear: '2003',
-    landmark: 'My House',
-    youCanFind: 'My items',
-    trails: 'The driveway',
-    icons: [{ iconName: 'Paddling-Red.svg' }, { iconName: 'RVCamping-Green.svg' }, { iconName: 'Playground-Blue.svg' }],
-    photos: [
-      {
-        photoPath: './photos/CABE.jpg',
-        alt: ''
-      },
-      {
-        photoPath: './photos/CACR.jpg',
-        alt: ''
-      },
-      {
-        photoPath: './photos/CACR.jpg',
-        alt: ''
-      },
-    ],
-    id: 0,
-    bucketListItems: []
-  };
 
+const park: Park = {
+  abbreviation: 'CABE',
+  parkName: 'Sample Data',
+  addresses: [
+    {
+      title: 'Secondary Address:',
+      addressLineOne: '1234 Main St',
+      city: 'Raleigh',
+      state: 'NC',
+      zipcode: 27606,
+      addressLineTwo: ''
+    },
+    {
+      title: 'Tertiary Address:',
+      addressLineOne: '1234 Main St',
+      city: 'Raleigh',
+      state: 'NC',
+      zipcode: 27606,
+      addressLineTwo: ''
+    },
+  ],
+  coordinates: { latitude: 35.2023, longitude: -78.9761 },
+  phone: 5555555555,
+  email: 'email@ncparks.gov',
+  website: 'ncparks.gov',
+  establishedYear: '2003',
+  landmark: 'My House',
+  youCanFind: 'My items',
+  trails: 'The driveway',
+  icons: [{ iconName: 'Paddling-Red.svg' }, { iconName: 'RVCamping-Green.svg' }, { iconName: 'Playground-Blue.svg' }],
+  photos: [
+    {
+      photoPath: './photos/CABE.jpg',
+      alt: ''
+    },
+    {
+      photoPath: './photos/CACR.jpg',
+      alt: ''
+    },
+    {
+      photoPath: './photos/CACR.jpg',
+      alt: ''
+    },
+  ],
+  id: 0,
+  bucketListItems: []
+};
+
+export const BucketListItem = () => {
+  const hasValidBucketListItem = park.bucketListItems?.[0]?.task?.length >= 0;
   return (
     <div className='my-2.5 flex items-start'>
-      {park.bucketListItems?.length<0 ? (
-        <FaRegCheckSquare data-testid="checked-icon" size={'24px'} strokeWidth={3} style={{ paddingRight: '5px', paddingTop: '3px' }} />
+      {hasValidBucketListItem ? (
+        <FaRegCheckSquare 
+          data-testid="checked-icon" 
+          size={'24px'} 
+          strokeWidth={3} 
+          style={{ paddingRight: '5px', paddingTop: '3px' }} 
+        />
       ) : (
-        <FaRegSquare data-testid="unchecked-icon" size={'24px'} strokeWidth={3} style={{ paddingRight: '5px', paddingTop: '3px' }} />
+        <FaRegSquare 
+          data-testid="unchecked-icon" 
+          size={'24px'} 
+          strokeWidth={3} 
+          style={{ paddingRight: '5px', paddingTop: '3px' }} 
+        />
       )}
       <div className='flex w-full flex-col justify-center'>
         <p>Participate</p>
