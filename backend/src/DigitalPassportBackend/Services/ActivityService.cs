78a144c8144a75da6e364aaeee765f10e9eda7ad
jwellio2 <jwellio2@ncsu.edu>
2025-02-21 14:32:56 -0500

    collect stamp endpoint, updated coordinates

--- backend/src/DigitalPassportBackend/Services/ActivityService.cs
+++ backend/src/DigitalPassportBackend/Services/ActivityService.cs
@@ -1,8 +1,13 @@
+
 using DigitalPassportBackend.Domain;
 using DigitalPassportBackend.Errors;
 using DigitalPassportBackend.Persistence.Repository;
 using DigitalPassportBackend.Security;
 using DigitalPassportBackend.Secutiry;
+
+using Microsoft.OpenApi.Extensions;
+
+using NetTopologySuite.Geometries;
 
 namespace DigitalPassportBackend.Services;
 
@@ -10,12 +15,76 @@ public class ActivityService(
     ICompletedBucketListItemRepository completedBucketListItemRepository,
     ICollectedStampRepository collectedStampRepository,
     IPrivateNoteRepository privateNoteRepository,
-    IParkVisitRepository parkVisitRepository) : IActivityService
+    IParkVisitRepository parkVisitRepository,
+    ILocationsRepository locationsRepository,
+    IUserRepository userRepository) : IActivityService
 {
     private readonly ICompletedBucketListItemRepository _completedBucketListItemRepository = completedBucketListItemRepository;
     private readonly ICollectedStampRepository _collectedStampRepository = collectedStampRepository;
     private readonly IPrivateNoteRepository _privateNoteRepository = privateNoteRepository;
     private readonly IParkVisitRepository _parkVisitRepository = parkVisitRepository;
+    private readonly ILocationsRepository _locationsRepository = locationsRepository;
+    private readonly IUserRepository _userRepository = userRepository;
+
+    public CollectedStamp CollectStamp(
+        string park_abbreviation,
+        double latitude,
+        double longitude,
+        double inaccuracyRadius,
+        string method,
+        DateTime? dateTime,
+        int userId)
+    {
+        // validation checks
+        // valid collection method
+        StampCollectionMethod methodEnum = StampCollectionMethod.location;
+        if (!Enum.TryParse<StampCollectionMethod>(method, true, out methodEnum))
+        {
+            throw new ServiceException(StatusCodes.Status412PreconditionFailed, "Stamp collection method is not valid.");
+        }
+        // not already collected
+        var park = _locationsRepository.GetByAbbreviation(park_abbreviation);
+        if (_collectedStampRepository.GetByParkAndUser(park.id, userId) != null)
+        {
+            throw new ServiceException(StatusCodes.Status409Conflict, "Stamp already collected for this park.");
+        }
+
+        // collect the stamp
+        var userLocation = GeometryFactory.Default.CreatePoint(new Coordinate(longitude, latitude));
+        if (method == StampCollectionMethod.location.GetDisplayName())
+        {
+            var locationWithInaccuracy = userLocation.Buffer(inaccuracyRadius);
+            if (park != null && park.boundaries!.Intersects(locationWithInaccuracy))
+            {
+                var collectedStamp = new CollectedStamp()
+                {
+                    location = userLocation,
+                    method = methodEnum,
+                    user = _userRepository.GetById(userId),
+                    park = park,
+                    createdAt = dateTime == null ? DateTime.UtcNow : dateTime.Value,
+                };
+                return _collectedStampRepository.Create(collectedStamp);
+            }
+            else
+            {
+                throw new ServiceException(StatusCodes.Status405MethodNotAllowed, "Your location doesn't appear to be at the specified park.");
+            }
+        }
+        else
+        {
+            var collectedStamp = new CollectedStamp()
+            {
+                location = userLocation,
+                method = methodEnum,
+                user = _userRepository.GetById(userId),
+                park = park,
+                createdAt = dateTime == null ? DateTime.UtcNow : dateTime.Value,
+            };
+            return _collectedStampRepository.Create(collectedStamp);
+        }
+    }
+
 
     public ParkActivity GetParkActivity(int locationId, int userId)
     {
--- backend/src/DigitalPassportBackend/Services/IActivityService.cs
+++ backend/src/DigitalPassportBackend/Services/IActivityService.cs
@@ -1,8 +1,13 @@
+
 using DigitalPassportBackend.Domain;
+
+using NetTopologySuite.Geometries;
 
 namespace DigitalPassportBackend.Services;
 
 public interface IActivityService
 {
+    CollectedStamp CollectStamp(string park_abbreviation, double latitude, double longitude, double inaccuracyRadius, string method, DateTime? dateTime, int userId);
+
     ParkActivity GetParkActivity(int locationId, int userId);
 }
