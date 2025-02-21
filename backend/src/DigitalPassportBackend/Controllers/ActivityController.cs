6eb458c84ff2186d6b8559d95fe1ec2d1bae38c8
jwellio2 <jwellio2@ncsu.edu>
2025-02-21 14:32:56 -0500

    collect stamp endpoint, updated coordinates

--- backend/src/DigitalPassportBackend/Controllers/ActivityController.cs
+++ backend/src/DigitalPassportBackend/Controllers/ActivityController.cs
@@ -4,6 +4,8 @@ using DigitalPassportBackend.Services;
 using Microsoft.OpenApi.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
+using NetTopologySuite.Geometries;
+using System.Text.Json;
 
 namespace DigitalPassportBackend.Controllers;
 
@@ -21,5 +23,36 @@ public class ActivityController(IActivityService activityService) : ControllerBa
     {
         var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
         return Ok(_activityService.GetParkActivity(locationId, userId));
+    }
+
+    [HttpPost("stamps/collect/{park_abbreviation}")]
+    // [Authorize(Roles = "visitor")]
+    public IActionResult CollectStamp(
+        string park_abbreviation, 
+        [FromBody] CollectStampRequest request)
+    {
+        var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+        return Ok(CollectStampResponse.FromDomain(_activityService.CollectStamp(park_abbreviation, request.latitude, request.longitude, request.inaccuracyRadius, request.method, request.dateTime, userId)));
+    }
+
+    public record CollectStampResponse(int id, DateTime createdAt, string method, string parkAbbreviation)
+    {
+        public static CollectStampResponse FromDomain(CollectedStamp stamp)
+        {
+            return new CollectStampResponse(
+                stamp.id,
+                stamp.createdAt,
+                stamp.method.GetDisplayName(),
+                stamp.park.parkAbbreviation
+            );
+        }
+    }
+
+    public record CollectStampRequest( double latitude, 
+        double longitude,
+        double inaccuracyRadius,
+        string method, 
+        DateTime? dateTime)
+    {
     }
 }
