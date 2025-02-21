b9bb5fc54c331b9b9807a1f327cb9a4888ab10a7
jwellio2 <jwellio2@ncsu.edu>
2025-02-20 23:03:28 -0500

    initializing with park boundaries

---
+++ backend/src/DigitalPassportBackend/Persistence/Database/Scripts/002_park_photos_data.sql
@@ -0,0 +1,47 @@
+INSERT INTO park_photos (id,photo,alt,created_at,updated_at,park) VALUES
+	 (2,'CHRO.jpg','Chimney Rock State Park main photo','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',3),
+	 (3,'CACR.jpg','Carvers Creek State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',4),
+	 (4,'CLNE.jpg','Cliffs of the Neuse State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',5),
+	 (5,'CRMO.jpg','Crowders Mountain State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',6),
+	 (6,'DISW.jpg','Dismal Swamp State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',7),
+	 (7,'ELKN.jpg','Elk Knob State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',8),
+	 (8,'ENRI.jpg','Eno River State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',9),
+	 (9,'FALA.jpg','Falls Lake State Recreation Area main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',10),
+	 (10,'FOFI.jpg','Fort Fisher State Recreation Area main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',11),
+	 (11,'FOMA.jpg','Fort Macon State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',12);
+INSERT INTO park_photos (id,photo,alt,created_at,updated_at,park) VALUES
+	 (12,'GOCR.jpg','Goose Creek State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',13),
+	 (14,'GORG.jpg','Gorges State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',15),
+	 (15,'GRMO.jpg','Grandfather Mountain State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',16),
+	 (16,'HABE.jpg','Hammocks Beach State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',17),
+	 (17,'HARO.jpg','Hanging Rock State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',18),
+	 (18,'HARI.jpg','Haw River State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',19),
+	 (19,'JORI.jpg','Jockey''s Ridge State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',20),
+	 (20,'JONE.jpg','Jones Lake State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',21),
+	 (21,'JORD.jpg','Jordan Lake State Recreation Area main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',22),
+	 (22,'KELA.jpg','Kerr Lake State Recreation Area main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',23);
+INSERT INTO park_photos (id,photo,alt,created_at,updated_at,park) VALUES
+	 (23,'LAJA.jpg','Lake James State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',24),
+	 (24,'LANO.jpg','Lake Norman State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',25),
+	 (25,'LAWA.jpg','Lake Waccamaw State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',26),
+	 (26,'LURI.jpg','Lumber River State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',27),
+	 (27,'MARI.jpg','Mayo River State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',28),
+	 (28,'MEMO.jpg','Medoc Mountain State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',29),
+	 (29,'MEMI.jpg','Merchants Millpond State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',30),
+	 (30,'MOMO.jpg','Morrow Mountain State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',31),
+	 (31,'MOJE.jpg','Mount Jefferson State Natural Area main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',32),
+	 (32,'MOMI.jpg','Mount Mitchell State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',33);
+INSERT INTO park_photos (id,photo,alt,created_at,updated_at,park) VALUES
+	 (33,'NERI.jpg','New River State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',34),
+	 (34,'OCMO.jpg','Occoneechee Mountain State Natural Area main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',35),
+	 (35,'PETT.jpg','Pettigrew State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',36),
+	 (36,'PIMO.jpg','Pilot Mountain State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',37),
+	 (37,'RARO.jpg','Raven Rock State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',38),
+	 (38,'REMO.jpg','Rendezvous Mountain main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',39),
+	 (39,'SILA.jpg','Singletary Lake State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',40),
+	 (40,'SOMO.jpg','South Mountains State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',41),
+	 (41,'STMO.jpg','Stone Mountain State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',42),
+	 (42,'WEWO.jpg','Weymouth Woods Sandhills Nature Preserve main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',43);
+INSERT INTO park_photos (id,photo,alt,created_at,updated_at,park) VALUES
+	 (43,'WIUM.jpg','William B. Umstead State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',44),
+	 (44,'CABE.jpg','Carolina Beach State Park main image','2025-01-01 00:00:00.000000','2025-01-01 00:00:00.000000',45);
