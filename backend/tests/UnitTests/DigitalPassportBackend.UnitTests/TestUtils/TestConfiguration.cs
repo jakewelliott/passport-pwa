83c809f526854cd402191b3005db9e0d7a4ec418
ymehra <ymehra@ncsu.edu>
2025-02-25 18:12:35 -0500

    add testing resources for UploadGeoJson tests

--- backend/tests/UnitTests/DigitalPassportBackend.UnitTests/TestUtils/TestConfiguration.cs
+++ backend/tests/UnitTests/DigitalPassportBackend.UnitTests/TestUtils/TestConfiguration.cs
@@ -9,20 +9,26 @@ namespace DigitalPassportBackend.UnitTests.TestUtils;
 [ExcludeFromCodeCoverage]
 public static class TestConfiguration
 {
+    public static readonly string TestingResourcePath = $"{GetProjectRoot()}\\backend\\tests\\UnitTests\\DigitalPassportBackend.UnitTests\\resources";
+
     public static IConfiguration GetConfiguration()
     {
-        var workspace = Environment.GetEnvironmentVariable("GITHUB_WORKSPACE");
-        var basePath = workspace.IsNullOrEmpty()
-            ? Directory.GetParent(Directory.GetCurrentDirectory())!
-                .Parent!.Parent!.Parent!.Parent!.Parent!.Parent!
-                .FullName
-            : workspace!;
 
         return new ConfigurationBuilder()
-            .AddIniFile(new PhysicalFileProvider(basePath, ExclusionFilters.None),
+            .AddIniFile(new PhysicalFileProvider(GetProjectRoot(), ExclusionFilters.None),
                 ".env",
                 false,
                 true)
             .Build();
+    }
+
+    private static string GetProjectRoot()
+    {
+        var workspace = Environment.GetEnvironmentVariable("GITHUB_WORKSPACE");
+        return workspace.IsNullOrEmpty()
+            ? Directory.GetParent(Directory.GetCurrentDirectory())!
+                .Parent!.Parent!.Parent!.Parent!.Parent!.Parent!
+                .FullName
+            : workspace!;
     }
 }
