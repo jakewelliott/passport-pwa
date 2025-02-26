30fb59de5f551e380a97854977789d6abbad29ac
V <vegamehra@gmail.com>
2025-02-25 19:41:28 -0500

    fix loading files

--- backend/tests/UnitTests/DigitalPassportBackend.UnitTests/TestUtils/TestFileHelper.cs
+++ backend/tests/UnitTests/DigitalPassportBackend.UnitTests/TestUtils/TestFileHelper.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics.CodeAnalysis;
 
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
 
 namespace DigitalPassportBackend.UnitTests.TestUtils;
 [ExcludeFromCodeCoverage]
@@ -8,10 +9,18 @@ public static class TestFileHelper
 {
     public static IFormFile Open(string fileName)
     {
-        var stream = new FileStream(Path.Combine(TestConfiguration.TestingResourcePath, fileName), FileMode.Open, FileAccess.Read)
+        var filePath = Path.Combine(TestConfiguration.TestingResourcePath, fileName);
+
+        var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read)
         {
             Position = 0
         };
-        return new FormFile(stream, 0, stream.Length, fileName, fileName);
+        var file = new FormFile(stream, 0, stream.Length, fileName, fileName)
+        {
+            Headers = new HeaderDictionary()
+        };
+        new FileExtensionContentTypeProvider().TryGetContentType(filePath, out var contentType);
+        file.ContentType = contentType ?? "application/octet-stream";
+        return file;
     }
 }
