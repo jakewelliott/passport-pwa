6ff77543a051d5ec370d0a16d153ff97b02a92d4
alwettre <alwettre@ncsu.edu>
2025-02-28 20:21:05 -0500

    added mock useBucketList hook

--- frontend/src/app/locations/detail-tabs.tsx
+++ frontend/src/app/locations/detail-tabs.tsx
@@ -4,29 +4,26 @@ import { LocationContact } from '@/app/locations/components/location-contact';
 import { LocationMiniTabBar } from '@/app/locations/components/minitab-bar';
 import { NotesMiniTab } from '@/app/locations/components/notes-minitab';
 import { PhotoGalleryMiniTab } from '@/app/locations/components/photos-minitab';
-import { useParams } from 'react-router-dom';
-
 import { LoadingPlaceholder } from '@/components/loading-placeholder';
-import { usePark, useParkActivity } from '@/hooks/queries/useParks';
-import { useUser } from '@/hooks/queries/useUser';
+import { usePark } from '@/hooks/queries/useParks';
+import { useParams } from 'react-router-dom';
+import { BucketListView } from './components/bucket-list-view';
 
 export default function DetailTabs() {
-  const { abbreviation } = useParams();
-  const parkAbbreviation = abbreviation as Uppercase<string>;
-  const { data: park, isLoading: isParkLoading } = usePark(parkAbbreviation);
-  const { data: user, isLoading: isUserLoading } = useUser();
-  const { data: parkActivity, isLoading: isActivityLoading } =
-    !isUserLoading && user?.role !== 'admin' ? useParkActivity(park?.id ?? 0) : { data: null, isLoading: false };
-  if (isParkLoading || isActivityLoading || !park) return <LoadingPlaceholder />;
-  return (
-    <>
-      <LocationContact park={park} parkActivity={parkActivity ?? undefined} />
-      <LocationActionBar park={park} />
-      <LocationMiniTabBar>
-        <DetailsMiniTab park={park} />
-        <PhotoGalleryMiniTab photos={park.photos} />
-        <NotesMiniTab abbreviation={park.abbreviation} parkId={park.id} />
-      </LocationMiniTabBar>
-    </>
-  );
+	const { abbreviation } = useParams();
+	const parkAbbreviation = abbreviation as Uppercase<string>;
+	const { data: park, isLoading: isParkLoading } = usePark(parkAbbreviation);
+	if (isParkLoading || !park) return <LoadingPlaceholder />;
+	return (
+		<>
+			<BucketListView park={park} />
+			<LocationContact park={park} />
+			<LocationActionBar park={park} />
+			<LocationMiniTabBar>
+				<DetailsMiniTab park={park} />
+				<PhotoGalleryMiniTab photos={park.photos} />
+				<NotesMiniTab abbreviation={park.abbreviation} parkId={park.id} />
+			</LocationMiniTabBar>
+		</>
+	);
 }
