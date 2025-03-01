6463efd07cb3564bc899540fafa215de46d687b3
jwellio2 <jwellio2@ncsu.edu>
2025-03-01 16:15:21 -0500

    fix update/create private note

--- backend/src/DigitalPassportBackend/Services/IActivityService.cs
+++ backend/src/DigitalPassportBackend/Services/IActivityService.cs
@@ -7,6 +7,5 @@ public interface IActivityService
     List<CollectedStamp> GetCollectedStamps(int userId);
     CollectedStamp CollectStamp(string park_abbreviation, double latitude, double longitude, double inaccuracyRadius, string method, DateTime? dateTime, int userId);
     ParkActivity GetParkActivity(int locationId, int userId);
-    PrivateNote CreatePrivateNote(string parkAbbr, int userId, string note);
-    PrivateNote UpdatePrivateNote(int noteId, int userId, string note);
+    PrivateNote CreateUpdatePrivateNote(string parkAbbr, int userId, string note, string updatedAt);
 }
