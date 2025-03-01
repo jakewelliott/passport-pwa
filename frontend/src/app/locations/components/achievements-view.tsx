8750f3afff06c386edd2eb8b6f25f4faad3e536a
jwellio2 <jwellio2@ncsu.edu>
2025-03-01 15:48:56 -0500

    checking/unchecking bucket list items

--- frontend/src/app/locations/components/achievements-view.tsx
+++ frontend/src/app/locations/components/achievements-view.tsx
@@ -1,6 +1,8 @@
 import type { Park, ParkActivity } from '@/lib/mock/types';
 import { FaRegCheckSquare, FaRegSquare, FaStamp } from 'react-icons/fa';
 import DateHelper from '@/lib/date-helper';
+import { useUpdateBucketListItem } from '@/hooks/queries/useBucketList';
+import { useLocation } from '@/hooks/useLocation';
 
 interface AchievementsViewProps {
   parkActivity: ParkActivity;
@@ -8,7 +10,18 @@ interface AchievementsViewProps {
 }
 
 const AchievementsView = ({ parkActivity, park }: AchievementsViewProps) => {
+  const useUpdateBucketListItemMutation = useUpdateBucketListItem();
+  const userLocation = useLocation();
 
+  function updateBucketListItem(id: number, status: string): void {
+    const useUpdateBucketListItemMutation = useUpdateBucketListItem(id);
+    useUpdateBucketListItemMutation.mutate({
+      latitude: userLocation.geopoint?.latitude ?? 0, 
+      longitude: userLocation.geopoint?.longitude ?? 0, 
+      status: status, 
+      updatedAt: new Date()
+    });
+  }
 
   return (
     <div className='flex flex-col gap-3' data-testid='achievements-view'>
@@ -29,9 +42,9 @@ const AchievementsView = ({ parkActivity, park }: AchievementsViewProps) => {
             {park.bucketListItems.map((item, index) => (
               <div key={index} className='top-0 flex'>
                 {parkActivity.completedBucketListItems.some(completed => completed.id === item.id) ? (
-                  <FaRegCheckSquare size={'17px'} strokeWidth={3} style={{ paddingRight: '5px', paddingTop: '5px' }} />
+                  <FaRegCheckSquare size={'17px'} strokeWidth={3} style={{ paddingRight: '5px', paddingTop: '5px' }} onClick={() => updateBucketListItem(item.id, 'not completed')} />
                 ) : (
-                  <FaRegSquare size={'17px'} strokeWidth={3} style={{ paddingRight: '5px', paddingTop: '5px' }} />
+                  <FaRegSquare size={'17px'} strokeWidth={3} style={{ paddingRight: '5px', paddingTop: '5px' }} onClick={() => updateBucketListItem(item.id, 'completed')} />
                 )}
                 <p data-testid={'BLI'}>
                   Bucket List Item:
