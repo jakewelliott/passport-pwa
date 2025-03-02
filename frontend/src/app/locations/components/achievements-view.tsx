5064200ba3e42a22c3568e3fa29634d69ed5504f
alwettre <alwettre@ncsu.edu>
2025-03-02 14:29:36 -0500

    added new bucketlist component back to achviementsview

--- frontend/src/app/locations/components/achievements-view.tsx
+++ frontend/src/app/locations/components/achievements-view.tsx
@@ -1,42 +1,41 @@
+import { BucketList } from '@/components/bucket-list';
 import { LoadingPlaceholder } from '@/components/loading-placeholder';
-import { useBucketList } from '@/hooks/queries/useBucketList';
 import { useParkActivity } from '@/hooks/queries/useParks';
 import DateHelper from '@/lib/date-helper';
 import type { Park } from '@/lib/mock/types';
 import { FaStamp } from 'react-icons/fa';
 
 interface AchievementsViewProps {
-  park: Park;
+	park: Park;
 }
 
 const StampView = ({ date }: { date?: string }) => {
-  const dateString =
-    date !== undefined
-      ? `Stamp collected on ${DateHelper.stringify(new Date(date)).replace(',', ' at')}`
-      : 'Stamp not yet collected';
+	const dateString =
+		date !== undefined
+			? `Stamp collected on ${DateHelper.stringify(new Date(date)).replace(',', ' at')}`
+			: 'Stamp not yet collected';
 
-  return (
-    <div className='my-2.5 flex items-start'>
-      <FaStamp size={'24px'} strokeWidth={3} style={{ paddingRight: '5px', paddingTop: '3px' }} />
-      <div className='flex w-full flex-col justify-center'>
-        <p>{dateString}</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className='my-2.5 flex items-start'>
+			<FaStamp size={'24px'} strokeWidth={3} style={{ paddingRight: '5px', paddingTop: '3px' }} />
+			<div className='flex w-full flex-col justify-center'>
+				<p>{dateString}</p>
+			</div>
+		</div>
+	);
 };
 
 const AchievementsView = ({ park }: AchievementsViewProps) => {
-  const { data: parkActivity, isLoading: isParkActivityLoading } = useParkActivity(park.id);
-  const { items, completed, toggleCompletion, isLoading: isBucketListLoading } = useBucketList(park.id);
+	const { data: parkActivity, isLoading: isParkActivityLoading } = useParkActivity(park.id);
 
-  if (isParkActivityLoading || isBucketListLoading || !parkActivity)
-    return <LoadingPlaceholder what='your achievements' />;
+	if (isParkActivityLoading || !parkActivity) return <LoadingPlaceholder what='your achievements' />;
 
-  return (
-    <div className='flex flex-col gap-3' data-testid='achievements-view'>
-      <StampView date={parkActivity.stampCollectedAt} />
-    </div>
-  );
+	return (
+		<div className='flex flex-col gap-3' data-testid='achievements-view'>
+			<StampView date={parkActivity.stampCollectedAt} />
+			<BucketList parkId={park.id} />
+		</div>
+	);
 };
 
 export default AchievementsView;
