da987f461096f713aca42a94798e50b5a3225a90
alwettre <alwettre@ncsu.edu>
2025-03-02 14:32:42 -0500

    added address functionality to bucketlist

--- frontend/src/components/bucket-list.tsx
+++ frontend/src/components/bucket-list.tsx
@@ -1,31 +1,37 @@
 import { useBucketList } from '@/hooks/queries/useBucketList';
-import { useParkById } from '@/hooks/queries/useParks';
+import { useParks } from '@/hooks/queries/useParks';
+import type { BucketListItem } from '@/lib/mock/types';
 import { BucketListItemView } from './bucket-list-item';
 import { LoadingPlaceholder } from './loading-placeholder';
 
 interface BucketListProps {
-  parkId?: number;
-  showAddress?: boolean;
+	parkId?: number;
+	showAddress?: boolean;
 }
 
 /** Leave parkId blank to show all bucket list items. */
 export const BucketList = ({ parkId, showAddress = false }: BucketListProps) => {
-  const { items, completed, toggleCompletion, isLoading } = useBucketList(parkId);
-  const { data: park } = useParkById(parkId ?? 0);
+	const { items, completed, toggleCompletion, isLoading } = useBucketList(parkId);
+	const { data: parks } = useParks();
 
-  if (isLoading || !items || !completed) return <LoadingPlaceholder what='Your bucket list' />;
+	if (isLoading || !items || !completed) return <LoadingPlaceholder what='Your bucket list' />;
 
-  return (
-    <div className='flex flex-col'>
-      {items.map((item) => (
-        <BucketListItemView
-          key={item.id}
-          item={item}
-          completion={completed?.find((completed) => completed.itemId === item.id)}
-          handler={() => toggleCompletion(item.id)}
-          address={showAddress ? park?.addresses[0].addressLineOne : undefined}
-        />
-      ))}
-    </div>
-  );
+	const addressHelper = (item: BucketListItem) => {
+		const park = parks?.find((park) => park.id === item.parkId);
+		return park?.addresses[0].addressLineOne;
+	};
+
+	return (
+		<div className='flex flex-col'>
+			{items.map((item) => (
+				<BucketListItemView
+					key={item.id}
+					item={item}
+					completion={completed?.find((completed) => completed.itemId === item.id)}
+					handler={() => toggleCompletion(item.id)}
+					address={showAddress ? addressHelper(item) : undefined}
+				/>
+			))}
+		</div>
+	);
 };
