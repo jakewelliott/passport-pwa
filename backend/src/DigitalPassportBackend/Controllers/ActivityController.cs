d741cdafe064f43fab7bfed4571cb486a298a557
alwettre <alwettre@ncsu.edu>
2025-03-02 21:21:08 -0500

    added migartion, boundary check, and backend calls

--- backend/src/DigitalPassportBackend/Controllers/ActivityController.cs
+++ backend/src/DigitalPassportBackend/Controllers/ActivityController.cs
@@ -69,10 +69,10 @@ public class ActivityController(IActivityService activityService) : ControllerBa
 
 		[HttpPost("bucketlist/{itemId}")]
 		[Authorize(Roles = "visitor")]
-		public IActionResult ToggleBucketListItemCompletion(int itemId, [FromBody] Point location)
+		public IActionResult ToggleBucketListItemCompletion(int itemId, [FromBody] double longitude, double latitude, double inaccuracyRadius)
 		{
 			var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
-			return Ok(_activityService.ToggleBucketListItemCompletion(itemId, userId, location));
+			return Ok(_activityService.ToggleBucketListItemCompletion(itemId, userId, longitude, latitude, inaccuracyRadius));
 		}
     
     public record CollectStampResponse(int id, DateTime createdAt, string method, string parkAbbreviation)
---
+++ backend/src/DigitalPassportBackend/Migrations/20250303020512_AddDeletedColumnToCompletedBucketListItems.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DigitalPassportBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeletedColumnToCompletedBucketListItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "deleted",
+                table: "completed_bucket_list_items",
+                type: "tinyint(1)",
+                nullable: true,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "deleted",
+                table: "completed_bucket_list_items");
+        }
+    }
+}
