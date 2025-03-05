79c1cb72563da590418a121cc28e40edaec292b9
alwettre <alwettre@ncsu.edu>
2025-03-04 20:56:03 -0500

    fixed stamp collected date (mostly)

--- frontend/src/app/locations/components/achievements-view.tsx
+++ frontend/src/app/locations/components/achievements-view.tsx
@@ -1,39 +1,39 @@
 import { BucketList } from '@/components/bucket-list';
 import { LoadingPlaceholder } from '@/components/loading-placeholder';
 import { StampCollectedOn } from '@/components/stamp-collected-on';
-import { useParkActivity } from '@/hooks/queries/useParks';
+import { useStamp } from '@/hooks/queries/useStamps';
 import { dbg } from '@/lib/debug';
 import type { CollectedStamp, Park } from '@/types';
 import { FaStamp } from 'react-icons/fa';
 
 interface AchievementsViewProps {
-  park: Park;
+	park: Park;
 }
 
 const StampView = ({ stamp }: { stamp?: CollectedStamp }) => {
-  return (
-    <div className='my-2.5 flex items-start'>
-      <FaStamp size={'24px'} strokeWidth={3} style={{ paddingRight: '5px', paddingTop: '3px' }} />
-      <div className='flex w-full flex-col justify-center'>
-        <StampCollectedOn stamp={stamp} />
-      </div>
-    </div>
-  );
+	return (
+		<div className='my-2.5 flex items-start'>
+			<FaStamp size={'24px'} strokeWidth={3} style={{ paddingRight: '5px', paddingTop: '3px' }} />
+			<div className='flex w-full flex-col justify-center'>
+				<StampCollectedOn stamp={stamp} />
+			</div>
+		</div>
+	);
 };
 
 const AchievementsView = ({ park }: AchievementsViewProps) => {
-  const { data: parkActivity, isLoading: isParkActivityLoading } = useParkActivity(park.id);
+	const { data: stamp, isLoading } = useStamp(park.abbreviation);
 
-  if (isParkActivityLoading || !parkActivity) return <LoadingPlaceholder what='your achievements' />;
+	if (isLoading || !stamp) return <LoadingPlaceholder what='your achievements' />;
 
-  dbg('MISC', 'parkActivity', parkActivity);
+	dbg('MISC', 'stamp', stamp);
 
-  return (
-    <div className='flex flex-col gap-3' data-testid='achievements-view'>
-      <StampView stamp={parkActivity.stampCollectedAt} />
-      <BucketList parkId={park.id} />
-    </div>
-  );
+	return (
+		<div className='flex flex-col gap-3' data-testid='achievements-view'>
+			<StampView stamp={stamp} />
+			<BucketList parkId={park.id} />
+		</div>
+	);
 };
 
 export default AchievementsView;
