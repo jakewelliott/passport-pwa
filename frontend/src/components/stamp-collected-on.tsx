96f772f8fc83821609aa1e16d166e5e49145a301
alwettre <alwettre@ncsu.edu>
2025-03-04 20:56:03 -0500

    fixed stamp collected date (mostly)

--- frontend/src/components/stamp-collected-on.tsx
+++ frontend/src/components/stamp-collected-on.tsx
@@ -1,10 +1,8 @@
 import DateHelper from '@/lib/date-helper';
 import type { CollectedStamp } from '@/types';
-
 export const StampCollectedOn = ({ stamp }: { stamp?: CollectedStamp }) => {
-  const dateString =
-    stamp !== null && stamp !== undefined
-      ? `Stamp collected on ${DateHelper.stringify(new Date(stamp.createdAt))}`
-      : 'Stamp not yet collected';
-  return <p>{dateString}</p>;
+	const d = new Date(stamp?.createdAt ?? '');
+	const dateString =
+		stamp !== null && stamp !== undefined ? `Stamp collected on ${DateHelper.stringify(d)}` : 'Stamp not yet collected';
+	return <p>{dateString}</p>;
 };
