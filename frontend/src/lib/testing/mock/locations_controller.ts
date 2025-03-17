d250811958d68c007be488000249e6fd2cb1cb18
alwettre <alwettre@ncsu.edu>
2025-03-17 14:57:23 -0400

    idk

--- frontend/src/lib/testing/mock/locations_controller.ts
+++ frontend/src/lib/testing/mock/locations_controller.ts
@@ -1,13 +1,13 @@
 import type { Park } from '@/types';
 import { parks } from './tables/index';
 
-export const Get = (parkAbbreviation: string): Park => {
+export const GetPark = (parkAbbreviation: string): Park => {
   const park = parks.find((park: Park) => park.abbreviation === parkAbbreviation);
   if (!park) throw new Error(`Park not found: ${parkAbbreviation}`);
   return park;
 };
 
-export const GetAll = (): Park[] => {
+export const GetAllParks = (): Park[] => {
   return parks;
 };
 
