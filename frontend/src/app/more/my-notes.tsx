061b54a0b53e50864a59c5ac72ada391db1cf1b1
alwettre <alwettre@ncsu.edu>
2025-03-17 16:44:28 -0400

    general note fix

--- frontend/src/app/more/my-notes.tsx
+++ frontend/src/app/more/my-notes.tsx
@@ -2,72 +2,65 @@ import { LoadingPlaceholder } from '@/components/loading-placeholder';
 import { useGetAllNotes } from '@/hooks/queries/useNotes';
 import { a11yOnClick } from '@/lib/a11y';
 import dateHelper from '@/lib/date-helper';
-import { useNavigate } from 'react-router-dom';
+import type { ParkNote } from '@/types';
+import { type NavigateFunction, useNavigate } from 'react-router-dom';
 import ListRow from '../../components/list-row';
 
-const generalNote = (
-	<ListRow>
-		<div className='flex flex-col gap-1'>
-			<h3>General Notes</h3>
-			<p className='mb-2 text-gray-500 text-sm'>
-				Last updated:{' '}
-				{/* {generalNotesUpdated != undefined ? dateHelper.toStringLong(new Date(generalNotesUpdated)) : 'Not Available'} */}
-			</p>
-			<p
-				className='overflow-wrap-anywhere line-clamp-3 max-w-full hyphens-auto break-words'
-				style={{ hyphens: 'auto' }}
-			>
-				{/* {generalNotes || 'No general notes yet'} */}
-			</p>
+const isGeneralNote = (note: ParkNote) => {
+	return note.parkAbbreviation === 'generalNotes';
+};
+
+
+const NoteRow = ({ note, navigate }: { note: ParkNote; navigate: NavigateFunction }) => {
+	const navigateTo = isGeneralNote(note)
+		? '/more/my-notes/general-notes'
+		: `/locations/${note.parkAbbreviation}?tab=notes`;
+
+	const title = isGeneralNote(note) ? 'General Notes' : note.parkAbbreviation;
+
+	return (
+		<div key={note.parkAbbreviation} {...a11yOnClick(() => navigate(navigateTo))} className='cursor-pointer'>
+			<ListRow>
+				<div className='flex flex-col gap-1'>
+					<h3>{title}</h3>
+					<p className='mb-2 text-gray-500 text-sm'>
+						Last updated: {note.updatedAt ? dateHelper.toStringLong(new Date(note.updatedAt)) : 'Not Available'}
+					</p>
+					<p
+						className='overflow-wrap-anywhere line-clamp-3 max-w-full hyphens-auto break-words'
+						style={{ hyphens: 'auto' }}
+					>
+						{note.note}
+					</p>
+				</div>
+			</ListRow>
 		</div>
-	</ListRow>
-);
+	);
+};
 
 export const MyNotes = () => {
 	const navigate = useNavigate();
-	const { data: remoteNotes, refetch, isLoading } = useGetAllNotes();
+	const { data: remoteNotes, isLoading } = useGetAllNotes();
+
+	const generalNote = remoteNotes?.find((note) => isGeneralNote(note));
+	const restOfNotes = remoteNotes?.filter((note) => !isGeneralNote(note)) || [];
+	const allNotes = [generalNote, ...restOfNotes];
 
 	// TODO: restore general note functionality
 	// we are going to use parkID of 0 for this
 
 	if (isLoading) return <LoadingPlaceholder what='notes' />;
 
+	console.log('ALL NOTES');
+	console.log(allNotes);
+
 	return (
 		<div className='container mx-auto px-4 py-4'>
 			<div className='space-y-4'>
-				<div {...a11yOnClick(() => navigate('/more/my-notes/general-notes'))} className='cursor-pointer'>
-					{generalNote}
-				</div>
-				{/* Park Notes section */}
-
-				{remoteNotes?.length === 0 ? (
+				{allNotes?.length === 0 ? (
 					<p className='text-gray-600'>No park notes found.</p>
 				) : (
-					remoteNotes?.map((park) => (
-						<div
-							key={park.parkAbbreviation}
-							{...a11yOnClick(() => navigate(`/locations/${park.parkAbbreviation}?tab=notes`))}
-							className='cursor-pointer'
-						>
-							<ListRow>
-								<div className='flex flex-col gap-1'>
-									<h3>{park.parkAbbreviation}</h3>
-									<p className='mb-2 text-gray-500 text-sm'>
-										Last updated:{' '}
-										{park.updatedAt
-											? dateHelper.toStringLong(new Date(park.updatedAt))
-											: 'Not Available'}
-									</p>
-									<p
-										className='overflow-wrap-anywhere line-clamp-3 max-w-full hyphens-auto break-words'
-										style={{ hyphens: 'auto' }}
-									>
-										{park.note}
-									</p>
-								</div>
-							</ListRow>
-						</div>
-					))
+					allNotes?.map((note) => note && <NoteRow key={note.parkAbbreviation} note={note} navigate={navigate} />)
 				)}
 			</div>
 		</div>
--- frontend/src/hooks/queries/useNotes.ts
+++ frontend/src/hooks/queries/useNotes.ts
@@ -1,17 +1,12 @@
 import { dbg } from '@/lib/debug';
 import { API_NOTES_URL, fetchGet, fetchPost } from '@/lib/fetch';
+import type { ParkNote } from '@/types';
 import { useMutation, useQuery } from '@tanstack/react-query';
-
-interface Note {
-  parkAbbreviation: string;
-  note: string;
-  updatedAt: string;
-}
 
 export const useNote = (parkId: number) => {
   dbg('HOOK', 'useNote', parkId);
 
-  return useQuery<Note>({
+  return useQuery<ParkNote>({
     queryKey: ['notes', parkId],
     queryFn: () => fetchGet(`${API_NOTES_URL}/${parkId}`),
   });
@@ -20,7 +15,7 @@ export const useNote = (parkId: number) => {
 export const useGetAllNotes = () => {
   dbg('HOOK', 'useGetAllNotes');
 
-  return useQuery<Note[]>({
+  return useQuery<ParkNote[]>({
     queryKey: ['notes'],
     queryFn: async () => await fetchGet(API_NOTES_URL),
   });
