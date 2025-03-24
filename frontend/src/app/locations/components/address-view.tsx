d83bff2af2d4523f89c8e6c6cdf11573022e05d5
jwellio2 <jwellio2@ncsu.edu>
2025-03-24 13:20:20 -0400

    fixes #167

--- frontend/src/app/locations/components/address-view.tsx
+++ frontend/src/app/locations/components/address-view.tsx
@@ -1,6 +1,7 @@
 import type { Address, Park } from '@/types';
 import { useState } from 'react';
 import { FiNavigation } from 'react-icons/fi';
+import { MdExpandLess, MdExpandMore } from 'react-icons/md';
 
 const ParkAddress = ({ address }: { address: Address }) => {
   return (
@@ -23,21 +24,38 @@ export const AddressView = ({ park }: { park: Park }) => {
     setShowAll(!showAll);
   };
 
-  const OtherAddresses = () =>
-    park.addresses.slice(1).map((address) => <ParkAddress key={address.title} address={address} />);
+  const addressesToShow = showAll ? park.addresses : park.addresses?.slice(0, 2);
 
   const Button = () => (
     <button type='button' onClick={toggle} className='ml-4 inline-flex items-center'>
-      {showAll ? 'Show Less' : 'Show More'}
+      {showAll ? (
+        <>
+          Show Less <MdExpandLess className='ml-1' />
+        </>
+      ) : (
+        <>
+          Show More <MdExpandMore className='ml-1' />
+        </>
+      )}
     </button>
   );
 
   return (
-    <div className='top-0 flex flex-col p-4' key={park.parkName}>
-      <FiNavigation size={'17px'} strokeWidth={3} style={{ paddingRight: '5px', paddingTop: '5px' }} />
-      <ParkAddress address={park.addresses[0]} />
-      {showAll && <OtherAddresses />}
-      {showButton && <Button />}
+    <div className='m-4 flex flex-col gap-3' data-testid='location-address'>
+      {park.addresses && park.addresses.length > 0 && (
+        <>
+          {addressesToShow.map((address) => (
+            <div className='top-0 flex' key={park.parkName}>
+            <FiNavigation size={'17px'} strokeWidth={3} style={{ paddingRight: '5px', paddingTop: '5px' }} />
+            <ParkAddress address={address} />
+            </div>
+          ))}
+          {park.addresses.length > 2 && (
+            <Button />
+          )}
+        </>
+      )}
     </div>
+    
   );
 };
