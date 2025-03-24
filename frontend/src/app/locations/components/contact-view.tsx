038b7c3ad431be2ad94c12aab9d77c462934db50
jwellio2 <jwellio2@ncsu.edu>
2025-03-24 14:04:54 -0400

    fixes #137 #138

--- frontend/src/app/locations/components/contact-view.tsx
+++ frontend/src/app/locations/components/contact-view.tsx
@@ -9,13 +9,13 @@ export const ContactView = ({ park }: { park: Park }) => {
       <div className='top-0 flex'>
         <FiNavigation size={'17px'} strokeWidth={3} style={{ paddingRight: '5px', paddingTop: '5px' }} />
         <p>
-          GPS: {park.coordinates.latitude}, {park.coordinates.longitude}
+          GPS: {park.coordinates.latitude.toFixed(4)}, {park.coordinates.longitude.toFixed(4)}
         </p>
       </div>
       {/* Phone */}
       <div className='top-0 flex'>
         <FiPhone size={'17px'} strokeWidth={3} style={{ paddingRight: '5px', paddingTop: '5px' }} />
-        <p>{park.phone}</p>
+        <p>{String(park.phone).replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3')}</p>
       </div>
       {/* Email */}
       <div className='top-0 flex'>
