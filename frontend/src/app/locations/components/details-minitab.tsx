8263dd97f5ec6ace3dc573da859c518c9feb7be0
jwellio2 <jwellio2@ncsu.edu>
2025-03-25 01:15:23 -0400

    fixes #169

--- frontend/src/app/locations/components/details-minitab.tsx
+++ frontend/src/app/locations/components/details-minitab.tsx
@@ -3,7 +3,7 @@ import type React from 'react';
 
 const Highlight = ({ title, children }: { title: string; children?: React.ReactNode }) => {
   return (
-    <p>
+    <p className={children ? 'mb-2' : ''}>
       <span className='green-text'>{title}:</span>&nbsp;&nbsp;{children}
     </p>
   );
@@ -13,7 +13,7 @@ const renderTrails = (trails: string) => {
   const trailLines = trails.split('\n').filter((line) => line.trim() !== ''); // Remove empty lines
 
   return (
-    <div className='flex flex-col gap-1 py-2'>
+    <div className='flex flex-col gap-1 pb-4'>
       {trailLines.map((line) => (
         <div className={line.charAt(0) !== '■' ? 'pl-5' : ''} key={line}>{line}</div>
       ))}
@@ -43,13 +43,13 @@ export const DetailsMiniTab = ({ park }: { park: Park }) => {
   return (
     <div className='mt-6 mb-6 flex flex-col'>
       <div className='gap-2 pr-6 pl-6'>
-        <Highlight title='Established'>{park.establishedYear}</Highlight>
-        <Highlight title='Landmark'>{park.landmark}</Highlight>
-        <Highlight title='You can find'>{park.youCanFind}</Highlight>
-        <div className='flex flex-col gap-1'>
+        {park.establishedYear && <Highlight title='Established'>{park.establishedYear}</Highlight>}
+        {park.landmark && <Highlight title='Landmark'>{park.landmark}</Highlight>}
+        {park.youCanFind && <Highlight title='You can find'>{park.youCanFind}</Highlight>}
+        {park.trails && (<div className='flex flex-col gap-1'>
           <Highlight title='Park trails' />
           {renderTrails(park.trails)}
-        </div>
+        </div>)}
       </div>
       <ParkIcons park={park} />
     </div>
