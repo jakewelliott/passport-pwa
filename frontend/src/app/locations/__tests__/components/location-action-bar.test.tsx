fde08987e4758047fe5b35a2797996619cee8013
ymehra <ymehra@ncsu.edu>
2025-03-25 18:06:14 -0400

    add LocationActionBar tests

---
+++ frontend/src/app/locations/__tests__/components/location-action-bar.test.tsx
@@ -0,0 +1,36 @@
+import { renderWithClient } from "@/lib/testing/test-wrapper";
+import { screen } from '@testing-library/react';
+import { describe, expect, it } from "vitest";
+import { LocationActionBar } from "../../components/location-action-bar";
+import { mockPark } from "@/lib/testing/mock";
+
+describe('LocationActionBar', () => {
+    it('should render', () => {
+        renderWithClient(<LocationActionBar park={mockPark} />);
+        expect(screen.getByTestId('location-action-bar')).toBeInTheDocument();
+    });
+
+    it('should contain coordinates link', () => {
+        renderWithClient(<LocationActionBar park={mockPark} />);
+        expect(screen.getByTestId('location-action-bar').innerHTML)
+            .contains(`href="https://www.google.com/maps/place/${mockPark.coordinates.latitude},${mockPark.coordinates.longitude}"`);
+    });
+
+    it('should contain telephone link', () => {
+        renderWithClient(<LocationActionBar park={mockPark} />);
+        expect(screen.getByTestId('location-action-bar').innerHTML)
+            .contains(`href="tel://${mockPark.phone}"`);
+    });
+
+    it('should contain website link', () => {
+        renderWithClient(<LocationActionBar park={mockPark} />);
+        expect(screen.getByTestId('location-action-bar').innerHTML)
+            .contains(`href="${mockPark.website}"`);
+    });
+
+    it('should contain email link', () => {
+        renderWithClient(<LocationActionBar park={mockPark} />);
+        expect(screen.getByTestId('location-action-bar').innerHTML)
+            .contains(`href="mailto:${mockPark.email}"`);
+    });
+});
