c1be325738fdb86f0cebe30dc47129e150e5f0e4
jwellio2 <jwellio2@ncsu.edu>
2025-03-25 13:34:36 -0400

    progress #147

---
+++ frontend/cache-files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PUBLIC_DIR="public"
+OUTPUT_TS="src/cached-files.ts"
+
+# Generate TS file with array export
+echo "export const cachedFiles = [" > $OUTPUT_TS
+find $PUBLIC_DIR -type f | sed 's/^public//' | awk '{printf "  '\''%s'\'',\n", $0}' >> $OUTPUT_TS
+echo "];" >> $OUTPUT_TS
+
+echo "Generated cached files list at $OUTPUT_TS"
--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,22 +1,26 @@
 const CACHE_NAME = 'nc-dpr-digital-passport-v0.1';
 
+import { cachedFiles } from '../src/cached-files';
+
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', (event) => {
   event.waitUntil(
     (async () => {
       const cache = await caches.open(CACHE_NAME);
-      cache.addAll(['./']);
+      cache.addAll(cachedFiles);
     })(),
   );
 });
 
 self.addEventListener('fetch', (event) => {
+  const requestURL = new URL(event.request.url);
+  const cacheKey = new Request(requestURL.pathname, event.request);
   event.respondWith(
     (async () => {
       const cache = await caches.open(CACHE_NAME);
 
       // Get the resource from the cache.
-      const cachedResponse = await cache.match(event.request);
+      const cachedResponse = await cache.match(cacheKey);
       if (cachedResponse) {
         return cachedResponse;
       }
