ad4a86a13dc02ac6a6f2f0059e75d25b10a5a302
jwellio2 <jwellio2@ncsu.edu>
2025-04-01 03:02:39 -0400

    fixed addresses not showing up

--- frontend/src/app/locations/components/address-view.tsx
+++ frontend/src/app/locations/components/address-view.tsx
@@ -31,7 +31,12 @@ export const AddressView = ({ park }: { park: Park }) => {
             {park.addresses && park.addresses.length > 0 && (
                 <>
                     {addressesToShow.map((address) => (
+                        <>
                         <GenericIcon key={park.parkName} name='location' text={address.title} />
+                        {address.addressLineOne && <p className='-mt-3 ml-7'>{address.addressLineOne}</p>}
+                        {address.addressLineTwo && <p className='-mt-3 ml-7'>{address.addressLineTwo}</p>}
+                        {address.city && <p className='-mt-3 ml-7'>{address.city}, {address.state} {address.zipcode}</p>}
+                        </>
                     ))}
                     {park.addresses.length > 2 && <Button />}
                 </>
